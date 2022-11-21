ca667b7c05dfe1c5495c722997fc2ef6d55c6351
Renato Ruppel <heruppel2@gmail.com>
2022-11-20 23:47:20 -0300

    chore(init): added packages and configuration needed

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final primary = const Color(0xFFFF941A);
+  static final secondary = const Color(0xFF585666);
+  static final grey = const Color(0xFF585666);
+  static final delete = const Color(0xFFE83F5B);
+  static final heading = const Color(0xFF585666);
+  static final body = const Color(0xFF706E7A);
+  static final stroke = const Color(0xFFE3E3E6);
+  static final shape = const Color(0xFFFAFAFC);
+  static final background = const Color(0xFFFFFFFF);
+  static final input = const Color(0xFFB1B0B8);
+}
