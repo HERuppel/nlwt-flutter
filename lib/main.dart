1006151f9274a1705759f149c6723acff5a8735e
Renato Ruppel <heruppel2@gmail.com>
2022-11-20 23:56:38 -0300

    feat(splash): created splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:nlwt/modules/splash/splash_page.dart';
+import 'package:nlwt/shared/themes/app_colors.dart';
 
 void main() {
-  runApp(MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: Scaffold(
-          appBar: AppBar(title: const Text("Payflow Flutter")),
-          body: Container())));
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Payflow",
+      theme: ThemeData(primaryColor: AppColors.primary),
+      home: const SplashPage(),
+    );
+  }
 }
