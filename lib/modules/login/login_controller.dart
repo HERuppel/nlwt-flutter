a60208f997e28939f26ecf2f16fdc207dbaf5a8f
Renato Ruppel <heruppel2@gmail.com>
2022-11-23 13:08:50 -0300

    feat(auth): added user return management

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:nlwt/shared/auth/auth_controller.dart';
+import 'package:nlwt/shared/models/user_model.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -11,7 +12,9 @@ class LoginController {
     ]);
     try {
       final response = await googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user =
+          UserModel(name: response!.displayName!, photoURL: response.photoUrl!);
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
       authController.setUser(context, null);
