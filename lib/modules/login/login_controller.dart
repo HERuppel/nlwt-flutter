633597fce1cec09d7bdc8cd973dc6c0a99de762b
Renato Ruppel <heruppel2@gmail.com>
2022-11-23 12:25:00 -0300

    feat(auth): added  signin redirect to routes

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,14 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:nlwt/shared/auth/auth_controller.dart';
 
 class LoginController {
-  Future<void> googleSignIn() async {
+  final authController = AuthController();
+
+  Future<void> googleSignIn(BuildContext context) async {
     GoogleSignIn googleSignIn = GoogleSignIn(scopes: [
       'email',
     ]);
     try {
       final response = await googleSignIn.signIn();
+      authController.setUser(context, response);
       print(response);
     } catch (error) {
+      authController.setUser(context, null);
       print(error);
     }
   }
