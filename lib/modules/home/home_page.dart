beb85a9d641bad116f8e882b022f21bc0d8c7e9d
Renato Ruppel <heruppel2@gmail.com>
2022-11-23 16:41:59 -0300

    feat(home): created appbar and bottom navigation bar

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nlwt/shared/themes/app_colors.dart';
+import 'package:nlwt/shared/themes/app_text_style.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,9 +8,69 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("HomePage"),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                  TextSpan(
+                      text: "Olá, ",
+                      style: TextStyles.titleRegular,
+                      children: [
+                        TextSpan(
+                            text: "Renato",
+                            style: TextStyles.titleBoldBackground)
+                      ]),
+                  style: TextStyles.titleRegular),
+              subtitle: Text("Mantenha suas contas em dia",
+                  style: TextStyles.captionShape),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+            ),
+          ),
+        ),
       ),
+      bottomNavigationBar: SizedBox(
+          height: 90,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.home,
+                    color: AppColors.primary,
+                  )),
+              GestureDetector(
+                onTap: () {
+                  print("CLicas");
+                },
+                child: Container(
+                  height: 56,
+                  width: 56,
+                  decoration: BoxDecoration(
+                      color: AppColors.primary,
+                      borderRadius: BorderRadius.circular(5)),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.add_box_outlined,
+                          color: AppColors.background)),
+                ),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon:
+                      Icon(Icons.description_outlined, color: AppColors.body)),
+            ],
+          )),
     );
   }
 }
