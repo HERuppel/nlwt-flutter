7e45ea7c643921844fd93cc6589551ec4e6bef04
Renato Ruppel <heruppel2@gmail.com>
2022-11-23 12:17:48 -0300

    feat(login): created controller and method for google sign in

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:nlwt/modules/login/login_controller.dart';
 import 'package:nlwt/shared/themes/app_colors.dart';
 import 'package:nlwt/shared/themes/app_images.dart';
 import 'package:nlwt/shared/themes/app_text_style.dart';
@@ -13,6 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -58,19 +60,9 @@ class _LoginPageState extends State<LoginPage> {
                     Padding(
                       padding:
                           const EdgeInsets.only(left: 40, right: 40, top: 40),
-                      child: SocialLoginButton(
-                        onTap: () async {
-                          GoogleSignIn googleSignIn = GoogleSignIn(scopes: [
-                            'email',
-                          ]);
-                          try {
-                            final response = await googleSignIn.signIn();
-                            print(response);
-                          } catch (error) {
-                            print(error);
-                          }
-                        },
-                      ),
+                      child: SocialLoginButton(onTap: () {
+                        controller.googleSignIn();
+                      }),
                     )
                   ],
                 ),
