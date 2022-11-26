2906829db4e6567f1a02aec327638749eb55436c
Renato Ruppel <heruppel2@gmail.com>
2022-11-25 23:49:44 -0300

    feat(bill): created model and controller functions

--- lib/modules/insert_bill/insert_bill_page.dart
+++ lib/modules/insert_bill/insert_bill_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:nlwt/modules/insert_bill/insert_bill_controller.dart';
 import 'package:nlwt/shared/themes/app_colors.dart';
 import 'package:nlwt/shared/themes/app_text_style.dart';
 import 'package:nlwt/shared/widgets/input_text/input_text.dart';
@@ -15,6 +16,7 @@ class InsertBillPage extends StatefulWidget {
 }
 
 class _InsertBillPageState extends State<InsertBillPage> {
+  final controller = InsertBillController();
   final moneyInputTextController =
       MoneyMaskedTextController(leftSymbol: "R\$", decimalSeparator: ",");
   final dueDateInputTextController = MaskedTextController(mask: "00/00/0000");
@@ -37,43 +39,65 @@ class _InsertBillPageState extends State<InsertBillPage> {
         elevation: 0,
         leading: BackButton(color: AppColors.input),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 93),
-              child: Text(
-                "Preencha os dados do boleto",
-                style: TextStyles.titleBoldHeading,
-                textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 93),
+                child: Text(
+                  "Preencha os dados do boleto",
+                  style: TextStyles.titleBoldHeading,
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            InputText(
-              label: "Nome do boleto",
-              icon: Icons.description_outlined,
-              onChanged: ((value) {}),
-            ),
-            InputText(
-                controller: dueDateInputTextController,
-                label: "Vencimento",
-                icon: FontAwesomeIcons.timesCircle,
-                onChanged: ((value) {})),
-            InputText(
-                controller: moneyInputTextController,
-                label: "Valor",
-                icon: FontAwesomeIcons.wallet,
-                onChanged: ((value) {})),
-            InputText(
-                controller: barcodeInputTextController,
-                label: "Código",
-                icon: FontAwesomeIcons.barcode,
-                onChanged: ((value) {})),
-          ],
+              const SizedBox(
+                height: 24,
+              ),
+              Form(
+                  key: controller.formKey,
+                  child: Column(
+                    children: [
+                      InputText(
+                        label: "Nome do boleto",
+                        icon: Icons.description_outlined,
+                        onChanged: ((value) {
+                          controller.onChange(name: value);
+                        }),
+                        validator: controller.validateName,
+                      ),
+                      InputText(
+                          controller: dueDateInputTextController,
+                          label: "Vencimento",
+                          icon: FontAwesomeIcons.timesCircle,
+                          validator: controller.validateDueDate,
+                          onChanged: ((value) {
+                            controller.onChange(dueDate: value);
+                          })),
+                      InputText(
+                          controller: moneyInputTextController,
+                          label: "Valor",
+                          icon: FontAwesomeIcons.wallet,
+                          validator: (_) => controller.validateMoney(
+                              moneyInputTextController.numberValue),
+                          onChanged: ((value) {
+                            controller.onChange(
+                                value: moneyInputTextController.numberValue);
+                          })),
+                      InputText(
+                          controller: barcodeInputTextController,
+                          label: "Código",
+                          icon: FontAwesomeIcons.barcode,
+                          validator: controller.validateBarcode,
+                          onChanged: ((value) {
+                            controller.onChange(barcode: value);
+                          })),
+                    ],
+                  ))
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: SetLabelButtons(
@@ -82,7 +106,10 @@ class _InsertBillPageState extends State<InsertBillPage> {
           Navigator.pop(context);
         },
         secondaryLabel: "Cadastrar",
-        secondaryOnPressed: () {},
+        secondaryOnPressed: () async {
+          await controller.createBill();
+          Navigator.pop(context);
+        },
         enableSecondaryColor: true,
       ),
     );
