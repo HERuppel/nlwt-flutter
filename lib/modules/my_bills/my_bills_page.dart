b36b7c1aa90be24d80ebecca4f595b553e8a331f
Renato Ruppel <heruppel2@gmail.com>
2022-11-26 11:41:29 -0300

    feat(bills): created bills info card to bills page

--- lib/modules/my_bills/my_bills_page.dart
+++ lib/modules/my_bills/my_bills_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:nlwt/shared/models/bill_model.dart';
 import 'package:nlwt/shared/themes/app_colors.dart';
 import 'package:nlwt/shared/themes/app_text_style.dart';
+import 'package:nlwt/shared/widgets/bill_info/bill_info.dart';
 import 'package:nlwt/shared/widgets/bill_list/bill_list.dart';
+import 'package:nlwt/shared/widgets/bill_list/bill_list_controller.dart';
 
 class MyBillsPage extends StatefulWidget {
   const MyBillsPage({super.key});
@@ -11,26 +14,52 @@ class MyBillsPage extends StatefulWidget {
 }
 
 class _MyBillsPageState extends State<MyBillsPage> {
+  final controller = BillListController();
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        Stack(
+          children: [
+            Container(
+              color: AppColors.primary,
+              height: 40,
+              width: double.maxFinite,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: ValueListenableBuilder<List<BillModel>>(
+                  valueListenable: controller.billsNotifier,
+                  builder: (_, bills, __) => BillInfo(size: bills.length)),
+            ),
+          ],
+        ),
         Padding(
-          padding: const EdgeInsets.only(top: 24),
-          child: Text(
-            "Meus Boletos",
-            style: TextStyles.titleBoldHeading,
+          padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
+          child: Row(
+            children: [
+              Text(
+                "Meus Boletos",
+                style: TextStyles.titleBoldHeading,
+              ),
+            ],
           ),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24),
+          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
           child: Divider(
             color: AppColors.stroke,
             height: 1,
             thickness: 1,
           ),
         ),
-        const BillList()
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: BillList(
+            controller: controller,
+          ),
+        )
       ],
     );
   }
--- lib/shared/widgets/bill_list/bill_list.dart
+++ lib/shared/widgets/bill_list/bill_list.dart
@@ -5,19 +5,18 @@ import 'package:nlwt/shared/widgets/bill_list/bill_list_controller.dart';
 import '../bill_tile/bill_tile.dart';
 
 class BillList extends StatefulWidget {
-  const BillList({super.key});
+  final BillListController controller;
+  const BillList({super.key, required this.controller});
 
   @override
   State<BillList> createState() => _BillListState();
 }
 
 class _BillListState extends State<BillList> {
-  final controller = BillListController();
-
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<List<BillModel>>(
-        valueListenable: controller.billsNotifier,
+        valueListenable: widget.controller.billsNotifier,
         builder: (_, bills, __) => Column(
               children: bills.map((bill) => BillTile(data: bill)).toList(),
             ));
